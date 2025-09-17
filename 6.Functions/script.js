822ed13a99382db818fa85fbdb13904ef2678ad9
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-17 09:52:25 +0530

    Adding fat arrow function

---
+++ 6.Functions/script.js
@@ -0,0 +1,13 @@
+//Function declaration
+// let fn = function(){
+//     console.log("Hello u just have entered function");
+// }
+
+// fn();
+
+//Arrow Function
+let fnc = () =>{
+    console.log("Fat arrow function");
+}
+
+fnc();
