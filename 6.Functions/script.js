0c79aa1b1d627d476e4c7a39bc420f67e78db8a6
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-17 11:58:39 +0530

    Adding ..

--- 6.Functions/script.js
+++ 6.Functions/script.js
@@ -22,3 +22,24 @@ function normal(name){               //name is the paramter
     console.log("Hello "+name);
 }
 normal("Himanshu");                  //Himanshu is the argument
+
+function addition(num1,num2){
+    let ans = num1 + num2;
+    console.log(ans);
+}
+addition(7,9);
+
+function add(num1 = 0,num2 =0){  //By default setting the value if arguments are not passed to avoid Nan.
+    return num1+num2;
+}
+
+let total = add(9,2);
+console.log(total);
+
+
+//When arguments are too many then use simply the ...val(rest operator) this avoid using too many parameters
+function abcd(...val){ 
+    console.log(val);
+}
+
+abcd(1,2,3,4,5,6,7,8,9); 
